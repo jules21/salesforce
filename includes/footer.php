304afa73736e755547e6de27e64c1ace5bfa4ca3
jules21 <jules21@github.com>
2018-05-14 08:23:41 +0200

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+
+    <div class="row">
+        <div class="col-md-12 footer">
+            <ul>
+                <li><a href="#">privacy</a></li>
+                <li><a href="#">Disclosure</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Trust</a></li>
+                
+            </ul><br>
+           <p> &copy; Copyright 2000-2018 salesforce.com, inc. <a href="#">All rights reserved</a>. Various trademarks held by their respective owners.</p>
+           <p>Salesforce.com EMEA Limited, village 9, floor 26 Salesforce Tower, 110 Bishopsgate, London, UK, EC2N 4AY. General Enquiries: +353 14403500 | Fax: +353 14403501 | Sales: 00800 7253 3333
+
+ </p>
+        </div>
+    </div>
---
+++ includes/header.php
@@ -0,0 +1,142 @@
+
+     <div class="col-md-12">
+          <img src="assets/images/logo-company.png" alt="logo">
+           <div class="theader">
+          
+                       
+            <input type="text" placeholder="Search">
+            
+            <a href="#">0080 7253 3333(FREEPHONE)</a>
+            <a href="#">CONTACT US</a>
+            <span>
+                
+                <i class="fa fa-address-book"></i>
+                
+                <a href="#">LOGIN</a>
+            
+            </span>
+            </div>
+            <hr>
+            <div class="navTop">
+      
+      
+       <ul><span id="navTleft"> 
+           <li><a href="#">PRODUCTS</a>
+           
+                  <div class="row tabs">
+            <div class="col-md-3 tabs-first">
+                    <div>
+                        <h4>Products Overview</h4>
+                        <h4>Pricing</h4>
+                        <hr>
+                        <h3><strong>SELL FASTER AND  <br>SMARTER</strong></h3>
+                        
+                      <div class="block"><h4>Salesforcw Lightning</h4>
+                        <p>Help reps sell the way they want, faster.</p>
+                        <h4><strong> LEARN HOW </strong>></h4><br>
+                        </div>
+                        <div class="block"><h4>Meet Salesforce Einstein</h4>
+                        <p>Introducing artificial Intelligence for everyone In your business.</p>
+                        <h4><strong> LEARN HOW ></strong></h4>
+                        </div>
+                        <div class="block"><h4>Salesforcw Lightning</h4>
+                        <p>Help reps sell the way they want, faster.</p>
+                        <h4><strong> LEARN HOW ></strong></h4> 
+                        </div>
+                    </div></div>
+         
+                <div class="col-md-3">
+                    
+                     <div class="block2">
+                        <h3><strong>SALES</strong></h3>
+                        <h4> Sales Cloud</h4>
+                        <p>Sales force automation and CRM</p>
+                        <h4> Salesforce CPQ</h4>
+                        <p>configure, price, quote(CPQ) and <br> billing automatiom</p>
+                        <h4>Data.com</h4>
+                        <p>B2B prospecting and data cleansing </p>                  
+                        
+                    </div>  <div class="block2">
+                        <h3><strong>SALES</strong></h3>
+                        <h4> Sales Cloud</h4>
+                        <p>Sales force automation and CRM</p>
+                        <h4> Salesforce CPQ</h4>
+                        <p>configure, price, quote(CPQ) and <br> billing automatiom</p>
+                        <h4>Data.com</h4>
+                        <p>B2B prospecting and data cleansing </p>                  
+                        
+                    </div>
+                    
+                  
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    
+                     <div class="block2">
+                        <h3><strong>SALES</strong></h3>
+                        <h4> Sales Cloud</h4>
+                        <p>Sales force automation and CRM</p>
+                        <h4> Salesforce CPQ</h4>
+                        <p>configure, price, quote(CPQ) and <br> billing automatiom</p>
+                        <h4>Data.com</h4>
+                        <p>B2B prospecting and data cleansing </p>                  
+                        
+                    </div>  <div class="block2">
+                        <h3><strong>SALES</strong></h3>
+                        <h4> Sales Cloud</h4>
+                        <p>Sales force automation and CRM</p>
+                        <h4> Salesforce CPQ</h4>
+                        <p>configure, price, quote(CPQ) and <br> billing automatiom</p>
+                        <h4>Data.com</h4>
+                        <p>B2B prospecting and data cleansing </p>                  
+                        
+                    </div>
+                    
+                  
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    
+                     <div class="block2">
+                        <h3><strong>SALES</strong></h3>
+                        <h4> Sales Cloud</h4>
+                        <p>Sales force automation and CRM</p>
+                        <h4> Salesforce CPQ</h4>
+                        <p>configure, price, quote(CPQ) and <br> billing automatiom</p>
+                        <h4>Data.com</h4>
+                        <p>B2B prospecting and data cleansing </p>                  
+                        
+                    </div>  <div class="block2">
+                        <h3><strong>SALES</strong></h3>
+                        <h4> Sales Cloud</h4>
+                        <p>Sales force automation and CRM</p>
+                        <h4> Salesforce CPQ</h4>
+                        <p>configure, price, quote(CPQ) and <br> billing automatiom</p>
+                        <h4>Data.com</h4>
+                        <p>B2B prospecting and data cleansing </p>                  
+                        
+                    </div>
+                    
+                  
+                    
+                </div>
+                
+        </div>
+           
+           </li>
+           <li><a href="#">SOLUTIONS</a></li>
+           <li><a href="#">SERVICES</a></li>
+           <li><a href="#">COMMUNITY</a></li>
+           </span>
+             <span id="navTright">   
+           <li><a href="#">CUSTOMERS</a></li>
+           <li><a href="#">EVENTS</a></li>
+           <li><a href="#">ABOUT US</a></li>
+           </span>
+       </ul>
+   <button class="btn btn-success">FREE TRIAL</button>
+      </div> 
+      
+       </div>
